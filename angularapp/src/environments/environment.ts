5707fd7c95847cba2a2e353582ab2a49035b337b
Iamneo <techadmin@iamneo.ai>
2023-10-03 12:29:11 +0000

    Backup Commit

--- angularapp/src/environments/environment.ts
+++ angularapp/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  baseUrl:"http://localhost:8080"
+  baseUrl:"http://8080-ffacafecdfdcfeaffccbfabccfbbbcaadfeecbce.premiumproject.examly.io"
 };
 
 /*
