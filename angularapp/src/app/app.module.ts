01b40f2a65ef67db9edbc871ad90bb026e37dbe7
Iamneo <techadmin@iamneo.ai>
2023-10-03 07:30:01 +0000

    Backup Commit

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -3,24 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AdminflowComponent } from './adminflow/adminflow.component';
-import { HomeComponent } from './adminflow/home/home.component';
-import { MenuComponent } from './adminflow/menu/menu.component';
-import { ModalComponent } from './adminflow/modal/modal.component';
-import { PaymentsComponent } from './adminflow/payments/payments.component';
-import { TablesComponent } from './adminflow/tables/tables.component';
-import { UsersComponent } from './adminflow/users/users.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    AdminflowComponent,
-    HomeComponent,
-    MenuComponent,
-    ModalComponent,
-    PaymentsComponent,
-    TablesComponent,
-    UsersComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
